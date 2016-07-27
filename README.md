f0f887c637a7e74c00e672a86f6831d73554c277
novitch <albanovitch@hotmail.fr>
2016-07-27 15:41:22 +0200

    adapted for Bfragilis table

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
+##########################################################################################
+### CREATE A COUNT MATRICE TABLE OF GROUPS OF GENES (KO?) BASED ON A GENE TABLE COUNT ###
+##########################################################################################
+Usage: perl pivot_table.pl -correlation -infile -output
+
+
 
--- pivot_table.pl
+++ pivot_table.pl
@@ -1,23 +1,5 @@
 #!/usr/bin/perl -w
 ###############################################################################
-# mega_pathway_abundance.pl
-###############################################################################
-#
-# DESCRIPTION: Parse blast output file in order to filter results on the best hit, % of alignment and % of similarity, evalue. Add the functional group associated
-#
-# INPUT:
-#
-# OUTPUT:
-#
-# USAGE: perl mega_pathway_abundance.pl -infile=  -correlation= -output=
-#
-#
-# VERSION:
-# - Author: Alban Mathieu (amathieu@enterome.com)
-# - Date: 23/03.2015
-# - Version 1
-#
-###############################################################################
 use strict;
 use Getopt::Long;
 my $usage = "Erreur!!  Usage: $0 $!\n";
@@ -46,7 +28,7 @@ if (! $megapathway) {
 }
 if (! $a) { print "No abundance table\n $usage\n"; exit;}
 
-
+print localtime()."\t"."pivot_table - Step 1/4\n";
 ###############################################################################
 ### correlation ###
 ###############################################################################
@@ -54,15 +36,8 @@ open (FILE1b, $megapathway) or die "pbm pathway file: $usage";
 while (<FILE1b>) {
     chomp;
     my @tab = split ("\t", $_);
-    if ($_ =~ /^#[^#]/) {
-        next;
-    }
-    elsif ($_ =~ /^##(.*)/) {
-        $nom = $1;
-    }
-    else{
-        $met{$tab[0]}{$nom} = 1;
-    }
+    my ($ko,$path) =~ /ko:(\S+)\s+(\S+)/;
+    $met{$ko}{$path} = 1;
 }
 
 close FILE1b;
@@ -70,7 +45,7 @@ close FILE1b;
 ###############################################################################
 #### Parse abundance table ###
 ###############################################################################
-print localtime()."\t"."functional_abundance - Step 2/4\n";
+print localtime()."\t"."pivot_table - Step 2/4\n";
 
 my @samples;
 
@@ -79,23 +54,18 @@ while (<FILE2>) {
     if ($_ !~ /^#/) {
         chomp;
         my @tab = split ("\t", $_);
-        if ($tab[0] eq "") {
-            for my $i (1..$#tab){
+        if ($tab[0] eq "GeneID") {
+            for my $i (3..$#tab){
                 push (@samples, $tab[$i])
             }
         }
         else{
-            $tab[0] =~/ko(\S+)/;
-            my $pat = $1;
-            if ($met{$pat}) {
-                foreach my $k (keys %{$met{$pat}}){
-                    for my $i (1..$#tab){
-                        my $s = $samples[$i-1];
-                        my $v = $tab[$i];
-                        $met2{$k}{$s} += $v;
-                    }
-                    
-                }
+            my $gene = $tab[0];
+            my $ko = $tab[1];
+            for my $i (3..$#tab){
+                my $s = $samples[$i-3];
+                my $v = $tab[$i];
+                $ko{$ko}{$s} += $v;
             }
         }
     }
@@ -106,17 +76,17 @@ close FILE2;
 ###############################################################################
 ### Writing ###
 ###############################################################################
-print localtime()."\t"."functional_abundance - Step 3/4\n";
+print localtime()."\t"."pivot_table - Step 3/4\n";
 ###############################################################################
 open(W3, ">$output_meta") or die "pbm output_met: $usage";
 
-print W3 "\t".join ("\t", @samples)."\n";
+print W3 "ko\t".join ("\t", @samples)."\n";
 
-foreach my $m (keys %met2){
+foreach my $m (keys %ko){
     my @valuesM;
     foreach my $s (@samples){
-        if ($met2{$m}{$s}) {
-            push (@valuesM, $met2{$m}{$s});
+        if ($ko{$m}{$s}) {
+            push (@valuesM, $ko{$m}{$s});
         }
         else{
             push (@valuesM, "0");
@@ -129,7 +99,7 @@ foreach my $m (keys %met2){
 close W3;
 
 ###############################################################################
-print localtime()."\t"."functional_abundance - Step 4/4\n";
+print localtime()."\t"."pivot_table - Step 4/4\n";
 
 print "Number of pathways = $tm\n";
 
